7c6c663b8ca5ed5ca4faf2cef10077e4ce8dfec6
YardenShoham <hrsi88@gmail.com>
2020-11-08 16:21:32 +0200

    🔥 Remove unused code

--- Content/components/expose-components.js
+++ Content/components/expose-components.js
@@ -4,18 +4,12 @@ import ReactDOMServer from 'react-dom/server';
 
 import HomeComponent from './home.jsx';
 import IndexComponent from './index.jsx';
-import { ServerStyleSheet } from 'styled-components';
-import { JssProvider, SheetsRegistry } from 'react-jss';
-import { renderStylesToString } from 'emotion-server';
 import Helmet from 'react-helmet';
 
 global.React = React;
 global.ReactDOM = ReactDOM;
 global.ReactDOMServer = ReactDOMServer;
 
-global.Styled = { ServerStyleSheet };
-global.ReactJss = { JssProvider, SheetsRegistry };
-global.EmotionServer = { renderStylesToString };
 global.Helmet = Helmet;
 
 global.Components = { HomeComponent, IndexComponent };
