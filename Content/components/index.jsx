311ccfddb01b5f039358b13eb90f8abdceb5f0f1
YardenShoham <hrsi88@gmail.com>
2020-11-11 20:36:01 +0200

    💄 Design header

--- Content/components/index.jsx
+++ Content/components/index.jsx
@@ -1,19 +1,42 @@
-﻿import React from 'react';
+﻿import React from "react";
 import Home from "./home.jsx";
 import List from "./list.jsx";
-import { Tabs, Flex } from '@bigcommerce/big-design';
+import { Tabs, H0, Box } from "@bigcommerce/big-design";
 
-export default function Index () {
-    const [activeTab, setActiveTab] = React.useState('tab1');
+export default function Index() {
+  const [activeTab, setActiveTab] = React.useState("tab1");
 
-    const items = [
-        { id: 'tab1', title: 'Home' },
-        { id: 'tab2', title: 'List' }
-    ];
-        
-        
-    return <><Flex justifyContent="center"><Tabs activeTab={activeTab} items={items} onTabClick={setActiveTab} /></Flex>
-            {activeTab === 'tab1' && <Home />}
-            {activeTab === 'tab2' && <List />}</>;
-    }
+  const items = [
+    { id: "tab1", title: <b>Summary</b> },
+    { id: "tab2", title: <b>Orders List</b> },
+  ];
 
+  return (
+    <React.Fragment>
+      <Box
+        backgroundColor="secondary20"
+        borderRadius="normal"
+        padding="xxxLarge"
+        paddingBottom="none"
+      >
+        <H0>Sample App</H0>
+        <Tabs
+          backgroundColor="secondary20"
+          borderRadius="normal"
+          activeTab={activeTab}
+          items={items}
+          onTabClick={setActiveTab}
+        />
+      </Box>
+      <Box
+        backgroundColor="secondary20"
+        border="box"
+        borderRadius="normal"
+        padding="medium"
+      >
+        {activeTab === "tab1" && <Home />}
+        {activeTab === "tab2" && <List />}
+      </Box>
+    </React.Fragment>
+  );
+}
