f327f23639db4ec56e352f6b15dc0b8e2d8e7332
YardenShoham <hrsi88@gmail.com>
2020-11-12 20:46:30 +0200

    🎨 Use react hooks

--- Content/components/list.jsx
+++ Content/components/list.jsx
@@ -1,155 +1,145 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   ProgressBar,
   Button,
   Text,
   Table,
-  Box,
-  H2,
+  Panel,
 } from "@bigcommerce/big-design";
 import { ApiService } from "../services/apiService";
 
-export default class List extends React.Component {
-  constructor(props) {
-    super(props);
+const List = () => {
+  const [loading, setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
 
-    this.state = {
-      isOrdersLoading: true,
-      orders: {
-        data: [],
-        pagination: {},
+  const tableHeaders = [
+    {
+      header: "Order ID",
+      hash: "id",
+      render: ({ id }) => id,
+    },
+    {
+      header: "Billing Name",
+      hash: "billing_address",
+      render: function ({ billing_address }) {
+        return `${billing_address.first_name} ${billing_address.last_name}`;
       },
-      tableHeaders: [
-        {
-          header: "Order ID",
-          hash: "id",
-          render: ({ id }) => id,
-        },
-        {
-          header: "Billing Name",
-          hash: "billing_address",
-          render: function ({ billing_address }) {
-            return `${billing_address.first_name} ${billing_address.last_name}`;
-          },
-        },
-        {
-          header: "Order Total",
-          hash: "total_inc_tax",
-          render: function ({ total_inc_tax }) {
-            return total_inc_tax;
-          },
-        },
-        {
-          header: "Order Status",
-          hash: "order_status",
-          render: function (data) {
-            let color;
-            if (data.status_id === 5) {
-              color = "danger";
-            } else if (data.status_id === 2 || data.status_id === 10) {
-              color = "success";
-            } else {
-              color = "secondary";
-            }
+    },
+    {
+      header: "Order Total",
+      hash: "total_inc_tax",
+      render: function ({ total_inc_tax, currency_code }) {
+        return new Intl.NumberFormat(undefined, {
+          style: "currency",
+          currency: currency_code,
+        }).format(total_inc_tax);
+      },
+    },
+    {
+      header: "Order Status",
+      hash: "order_status",
+      render: function (data) {
+        let color;
+        if (data.status_id === 5) {
+          color = "danger";
+        } else if (data.status_id === 2 || data.status_id === 10) {
+          color = "success";
+        } else {
+          color = "secondary";
+        }
 
-            return <Text color={color}>{data.status}</Text>;
-          },
-        },
-        {
-          header: "Actions",
-          hash: "actions",
-          render: function (data) {
-            if (data.status_id !== 5) {
-              return (
-                <Button
-                  actionType="destructive"
-                  onClick={(e) => this.cancelOrder(data.id, e)}
-                >
-                  Cancel
-                </Button>
-              );
-            }
-          }.bind(this),
-        },
-      ],
-    };
-  }
+        return <Text color={color}>{data.status}</Text>;
+      },
+    },
+    {
+      header: "Actions",
+      hash: "actions",
+      render: function (data) {
+        if (data.status_id !== 5) {
+          return (
+            <Button
+              actionType="destructive"
+              onClick={(e) => cancelOrder(data.id, e)}
+            >
+              Cancel
+            </Button>
+          );
+        }
+      },
+    },
+  ];
 
-  UNSAFE_componentWillMount() {
-    this.loadOrders();
-  }
+  const handleOrdersResponse = (ordersData) => {
+    setLoading(false);
+    setOrders(ordersData);
+  };
 
-  loadOrders() {
-    ApiService.getOrders({
+  const loadOrders = async () => {
+    const { data: ordersData } = await ApiService.getOrders({
       limit: 5,
-    }).then(this.handleOrdersResponse.bind(this));
-  }
-
-  handleOrdersResponse(response) {
-    this.setState({
-      isOrdersLoading: false,
-      orders: {
-        data: response.data,
-      },
     });
-  }
+    handleOrdersResponse(ordersData);
+  };
 
-  cancelOrder(orderId) {
+  const hasOrders = () => {
+    return orders.length > 0;
+  };
+
+  const cancelOrder = async (orderId) => {
     const newOrderData = { status_id: 5 };
 
-    this.setState({
-      isOrdersLoading: true,
-    });
+    setLoading(true);
+    await ApiService.updateOrder(orderId, newOrderData);
+    loadOrders();
+  };
 
-    ApiService.updateOrder(orderId, newOrderData).then(
-      this.loadOrders.bind(this)
-    );
-  }
+  useEffect(() => {
+    loadOrders();
+  }, []);
 
-  hasOrders() {
-    return this.state.orders.data.length > 0;
-  }
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPageOptions] = useState([5, 10, 20, 30]);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [currentItems, setCurrentItems] = useState([]);
 
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="card">
-              <Box
-                backgroundColor="secondary10"
-                border="box"
-                borderRadius="normal"
-                padding="medium"
-              >
-                <H2>List Orders</H2>
-              </Box>
+  const onItemsPerPageChange = (newRange) => {
+    setCurrentPage(1);
+    setItemsPerPage(newRange);
+  };
 
-              <Box
-                backgroundColor="white"
-                border="box"
-                borderRadius="normal"
-                padding="medium"
-              >
-                {this.state.isOrdersLoading ? (
-                  <ProgressBar />
-                ) : this.hasOrders() ? (
-                  <section>
-                    <Table
-                      columns={this.state.tableHeaders}
-                      items={this.state.orders.data}
-                    />
-                  </section>
-                ) : (
-                  <section>
-                    <div className="emptyTable">No orders exist yet!</div>
-                  </section>
-                )}
-              </Box>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+  useEffect(() => {
+    const maxItems = currentPage * itemsPerPage;
+    const lastItem = Math.min(maxItems, orders.length);
+    const firstItem = Math.max(0, maxItems - itemsPerPage);
+
+    setCurrentItems(orders.slice(firstItem, lastItem));
+  }, [currentPage, itemsPerPage]);
+
+  return (
+    <Panel header="Orders">
+      {loading ? (
+        <ProgressBar />
+      ) : hasOrders() ? (
+        <Table
+          keyField="id"
+          columns={tableHeaders}
+          items={orders}
+          itemName="Orders"
+          stickyHeader
+          pagination={{
+            currentPage,
+            totalItems: orders.length,
+            onPageChange: setCurrentPage,
+            itemsPerPageOptions,
+            onItemsPerPageChange,
+            itemsPerPage,
+          }}
+        />
+      ) : (
+        <Text bold={true}>No orders exist yet!</Text>
+      )}
+    </Panel>
+  );
+};
+
+export default List;
